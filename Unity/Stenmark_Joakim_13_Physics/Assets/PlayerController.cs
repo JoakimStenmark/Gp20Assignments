9c32b6148be4250920e1ee04ef92cc7e9c5849d9
JoakimStenmark <70517100+JoakimStenmark@users.noreply.github.com>
2020-10-20 15:13:28 +0200

    Assignment 13 done

---
+++ Unity/Stenmark_Joakim_13_Physics/Assets/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    Rigidbody2D rb2d;
+    Vector2 movement = new Vector2();
+    bool grounded = false;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        if (x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        movement = new Vector2(x * speed, rb2d.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            //Debug.Log("jump");
+            //rb2d.AddForce (new Vector2(0, jumpForce));
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = movement;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+
+        }
+    }
+}
